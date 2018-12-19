94ef7846ae2baa618dabe35ef3b3dd7176f8448b
Yoshiya <yohno@stuy.edu>
2018-12-18 22:54:58 -0500

    well I did some work jahaha

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,5 +1,9 @@
 #include "pipe_networking.h"
 
+#define PUBLIC_PIPE "jeff"
+#define PIPELEN     16
+#define CONFIRM     "YAHOO"
+    // yes I know this was defined for us
 
 /*=========================
   server_handshake
@@ -11,6 +15,37 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+    int read_result;
+
+    if(mkfifo("jeff", 0755)) {
+        printf("[SERVER]\tError creating public pipe:\n\t%s\n\n",
+                    strerror(errno));
+        exit(1);
+    }
+
+    int public_pipe = open(PUBLIC_PIPE, O_RDONLY);
+    // block here until the pipe is opened at the other end
+    if(public_pipe == -1) {
+        printf("[SERVER]\tError opening public pipe:\n\t%s\n\n",
+                    strerror(errno));
+        exit(1);
+    }
+
+    // part 1: client sends downstream pipe
+    char down_name[PIPELEN];
+    read_result = read(public_pipe, down_name, PIPELEN - 1);
+    if(read_result == -1) {
+        printf("[SERVER]\tError reading downstream pipe name:\n\t%s\n\n",
+                    strerror(errno));
+        exit(1);
+    }
+    down_name[read_result + 1] = '\0';
+
+    // part 2: open downstream pipe and send upstream pipe
+    *to_client = open(down_name, O_WRONLY);
+    char up_name[PIPELEN];
+
+    // part 3: confirm upstream pipe
     return 0;
 }
 
